aa903cccf19da409d3153abafd2a8462e1ec522a
ahmed <ahsan.elm.39@gmail.com>
2025-09-25 20:10:15 +0300

    improve

--- src/app/sections/Btn.tsx
+++ src/app/sections/Btn.tsx
@@ -3,7 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 const Btn = ({bg,text}:{text:string,bg:string}) => {
   return (
-    <div className={`get-box  px-3 py-2 font-medium flex gap-4 items-center cursor-pointer ${bg == 'white' ? "max-lg:hidden":""} w-fit bg-[${bg}]`}>
+    <div className={`get-box  px-3 py-2 font-medium flex gap-4 items-center cursor-pointer ${bg == 'white' ? "bg-white max-lg:hidden":""} w-fit bg-[#ffc221]`}>
         <span className='text-black font-bold text-sm tracking-tight'>{text}</span>
         <div className='bg-black text-white px-2 py-1'>
             <FontAwesomeIcon icon={faArrowRight} className='icon text-sm -rotate-40 transition-all'/>
